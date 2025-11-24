3c6c54c3e562d63c63c87fe457ffe4ac5705003b
Olga Davydova <olga_davydova1@epam.com>
2025-11-24 19:38:25 +0400

    Fixed long numbers format for objects and arrays

--- web/frontend/src/app/shared/utils/digitGrouping.ts
+++ web/frontend/src/app/shared/utils/digitGrouping.ts
@@ -1,6 +1,6 @@
 export function formatArray(array: any[], locale: string) {
   return array.map(item => {
-    if (['number', 'string'].includes(typeof item) && Math.abs(item) >= 1000) {
+    if (Math.abs(item) >= 1000 && Math.abs(item) <= Number.MAX_SAFE_INTEGER) {
       return parseFloat(item).toLocaleString(locale);
     } else if (item && typeof item === 'object') {
       return formatObject(item, locale);
@@ -16,7 +16,7 @@ export function formatObject(object: object, locale: string) {
   return Object.fromEntries(
     Object.entries(object).map(([key, value]) => {
       let formattedValue = value;
-      if (['number', 'string'].includes(typeof value) && Math.abs(value) >= 1000) {
+      if (Math.abs(value) >= 1000 && Math.abs(value) <= Number.MAX_SAFE_INTEGER) {
         formattedValue = parseFloat(value).toLocaleString(locale);
       } else if (value && typeof value === 'object') {
         formattedValue = formatObject(value, locale);
